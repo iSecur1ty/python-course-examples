73e105a336cfeef4d5ba25e43d6c369326bb3cbb
iSecur1ty <projects@isecur1ty.org>
2016-02-18 03:25:56 +0200

    Create python-example1.py

---
+++ python-example1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+
+username = raw_input("Please Enter your Username : ")
+password = raw_input("Please Enter your Password : ")
+
+uname = "Admin"
+password2  = "Admin123"
+
+if username == uname and password == password2:
+ print "Login Done !"
+else:
+ print "Please Enter your data again !"
